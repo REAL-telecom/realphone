e56d66dd8e7ec50a2ed90d99331d7ea5808bc999
Konstantin Leontyev <K.A.Leontyev@yandex.ru>
2025-10-03 17:00:44 +0400

    feat(project): initial commit

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import { checker } from 'vite-plugin-checker';
+import readableClassnames from 'vite-plugin-readable-classnames';
+import sassDts from 'vite-plugin-sass-dts';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    checker({
+      typescript: true,
+    }),
+    react(),
+    readableClassnames(),
+    sassDts({
+      enabledMode: ['development'],
+      esmExport: true,
+    }),
+    tsconfigPaths(),
+  ],
+  server: {
+    open: true,
+  },
+});
